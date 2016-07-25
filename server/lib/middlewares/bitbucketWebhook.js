022f65a07a2d3a963d65c88b349ce1f3bb7f86c3
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-07-25 11:57:39 +0300

    fixed branch detection for hg

--- server/lib/middlewares/bitbucketWebhook.js
+++ server/lib/middlewares/bitbucketWebhook.js
@@ -1,17 +1,17 @@
 import ip from 'ip';
-import { ArgumentError, UnauthorizedError } from '../errors';
+import {ArgumentError, UnauthorizedError} from '../errors';
 
-const parse = (headers, { push = {}, repository = {}, actor = {} }) => {
-  if(push.changes&&push.changes.length>0&&push.changes[0].new) {
-    let details = push.changes[0].new;
+const parse = (headers, {push = {}, repository = {}, actor = {}}) => {
+  if (push.changes && push.changes.length > 0 && push.changes[0].new) {
+    const details = push.changes[0].new;
     let diff = null;
-    if(push.changes[0].links&&push.changes[0].links.diff)
+    if (push.changes[0].links && push.changes[0].links.diff)
       diff = push.changes[0].links.diff.href.split('/').pop();
     return {
       id: headers['x-hook-uuid'],
       event: headers['x-event-key'],
-      branch: details.type=='branch'?details.name:'',
-      commits:push.changes[0].commits,
+      branch: (details.type === 'branch' || details.type === 'named_branch') ? details.name : '',
+      commits: push.changes[0].commits,
       repository: repository.full_name,
       user: actor.display_name,
       diff: diff,
@@ -22,18 +22,18 @@ const parse = (headers, { push = {}, repository = {}, actor = {} }) => {
       id: headers['x-hook-uuid'],
       event: headers['x-event-key'],
       branch: '',
-      commits:[],
+      commits: [],
       repository: repository.full_name,
       user: actor.display_name,
       sha: '',
-      diff:''
+      diff: ''
     }
   }
 };
-const getIpInRange = (currIp)=>{
+const getIpInRange = (currIp)=> {
   return ip.cidrSubnet('131.103.20.160/27').contains(currIp)
-      ||ip.cidrSubnet('165.254.145.0/26').contains(currIp)
-      ||ip.cidrSubnet('104.192.143.0/24').contains(currIp);
+    || ip.cidrSubnet('165.254.145.0/26').contains(currIp)
+    || ip.cidrSubnet('104.192.143.0/24').contains(currIp);
 };
 module.exports = () => (req, res, next) => {
   if (!req.headers['x-hook-uuid']) {
@@ -45,8 +45,7 @@ module.exports = () => (req, res, next) => {
   if (!getIpInRange(req.headers['x-forwarded-for'])) {
     return next(new ArgumentError('The Bitbucket delivery ip is not correct.'));
   }
-  console.log(JSON.stringify(req.headers));
-  console.log(JSON.stringify(req.body));
+
   req.webhook = parse(req.headers, req.body);
   return next();
 };
