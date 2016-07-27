4240dfee3e471f920abd4dafea3a431a449136ba
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-07-27 12:16:55 +0300

    ipv6 support added

--- server/lib/middlewares/bitbucketWebhook.js
+++ server/lib/middlewares/bitbucketWebhook.js
@@ -33,9 +33,20 @@ const parse = (headers, {push = {}, repository = {}, actor = {}}) => {
 const getIpInRange = (currIp)=> {
   const address = ipaddr.parse(currIp);
 
-  return address.match(ipaddr.parseCIDR('131.103.20.160/27'))
-    || address.match(ipaddr.parseCIDR('165.254.145.0/26'))
-    || address.match(ipaddr.parseCIDR('104.192.143.0/24'));
+  if (ipaddr.IPv4.isValid(currIp)) {
+    return address.match(ipaddr.parseCIDR('131.103.20.160/27'))
+      || address.match(ipaddr.parseCIDR('165.254.145.0/26'))
+      || address.match(ipaddr.parseCIDR('104.192.143.0/24'));
+  }
+  else if (ipaddr.IPv6.isValid(currIp)) {
+    return address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:8367:14a0/123'))
+      || address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:a5fe:9100/122'))
+      || address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:68c0:8f00/120'));
+  }
+  else {
+    return false;
+  }
+
 };
 module.exports = () => (req, res, next) => {
   if (!req.headers['x-hook-uuid']) {
