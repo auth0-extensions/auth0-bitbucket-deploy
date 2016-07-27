5877fe734f724e1a3c5e5e376a4e28b50ceeb91d
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2016-07-27 12:38:47 +0300

    ipv6 address check

--- server/lib/middlewares/bitbucketWebhook.js
+++ server/lib/middlewares/bitbucketWebhook.js
@@ -31,22 +31,21 @@ const parse = (headers, {push = {}, repository = {}, actor = {}}) => {
   }
 };
 const getIpInRange = (currIp)=> {
-  const address = ipaddr.parse(currIp);
+  let address;
 
   if (ipaddr.IPv4.isValid(currIp)) {
-    return address.match(ipaddr.parseCIDR('131.103.20.160/27'))
-      || address.match(ipaddr.parseCIDR('165.254.145.0/26'))
-      || address.match(ipaddr.parseCIDR('104.192.143.0/24'));
+    address = ipaddr.IPv4.parse(currIp)
   }
   else if (ipaddr.IPv6.isValid(currIp)) {
-    return address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:8367:14a0/123'))
-      || address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:a5fe:9100/122'))
-      || address.match(ipaddr.parseCIDR('2002:0:0:0:0:0:68c0:8f00/120'));
+    address = ipaddr.IPv6.parse(currIp).toIPv4Address();
   }
   else {
     return false;
   }
 
+  return address.match(ipaddr.parseCIDR('131.103.20.160/27'))
+    || address.match(ipaddr.parseCIDR('165.254.145.0/26'))
+    || address.match(ipaddr.parseCIDR('104.192.143.0/24'));
 };
 module.exports = () => (req, res, next) => {
   if (!req.headers['x-hook-uuid']) {
@@ -56,7 +55,7 @@ module.exports = () => (req, res, next) => {
     return next(new ArgumentError('The Bitbucket event name is missing.'));
   }
   if (!getIpInRange(req.headers['x-forwarded-for'])) {
-    return next(new ArgumentError('The Bitbucket delivery ip is not correct.'));
+    return next(new ArgumentError(`The Bitbucket delivery ip (${req.headers['x-forwarded-for']}) is not correct.`));
   }
 
   req.webhook = parse(req.headers, req.body);
