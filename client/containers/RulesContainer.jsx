0f9a6a495b9ab40f7497ae390ee6501b8afca63d
Daniela Borges <alunassertiva@gmail.com>
2016-11-30 11:58:48 +0000

    use extension ui

--- client/containers/RulesContainer.jsx
+++ client/containers/RulesContainer.jsx
@@ -1,10 +1,10 @@
 import React, { PropTypes, Component } from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 import connectContainer from 'redux-static';
+import { Error, LoadingPanel } from 'auth0-extension-ui';
 
 import { ruleActions } from '../actions';
 
-import { Error, LoadingPanel } from '../components/Dashboard';
 import RulesTable from '../components/RulesTable';
 
 export default connectContainer(class extends Component {
